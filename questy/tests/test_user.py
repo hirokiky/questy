60c93cd5064c2400278b73be8a5c1bd64e6a7bd0
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-11-02 01:42:18 +0900

    Added stream function for following commented pages

--- questy/tests/test_user.py
+++ questy/tests/test_user.py
@@ -5,10 +5,10 @@ from pyramid import testing
 from questy.models import DBSession
 
 
-class TestFollowingStream(unittest.TestCase):
+class TestFollowingArrivalStream(unittest.TestCase):
     def _callFUT(self, *args, **kwargs):
-        from questy.user import following_stream
-        return following_stream(*args, **kwargs)
+        from questy.user import following_arrival_stream
+        return following_arrival_stream(*args, **kwargs)
 
     def setUp(self):
         from questy.testing import setup_db
@@ -44,11 +44,63 @@ class TestFollowingStream(unittest.TestCase):
         user1 = User(user_id=1)
         user2 = User(user_id=2)
 
-        page = Page()
+        page = Page(page_id=1)
         page.arrivals = [arrival]
         user2.arrivals = [arrival]
 
         DBSession.add(arrival)
+        DBSession.add(user1)
+        DBSession.add(user2)
+        DBSession.add(page)
+
+        actual = self._callFUT(user1)
+        self.assertEqual(len(actual), 0)
+
+
+class TestFollowingCommentStream(unittest.TestCase):
+    def _callFUT(self, *args, **kwargs):
+        from questy.user import following_comment_stream
+        return following_comment_stream(*args, **kwargs)
+
+    def setUp(self):
+        from questy.testing import setup_db
+        setup_db()
+
+    def tearDown(self):
+        DBSession.remove()
+        testing.tearDown()
+
+    def test__following_commented(self):
+        from questy.models import User, Comment, Page
+        comment = Comment()
+        follower = User(user_id=1)
+        following = User(user_id=2)
+
+        page = Page(page_id=1)
+        page.comments = [comment]
+        follower.followings = [following]
+        following.comments = [comment]
+
+        DBSession.add(comment)
+        DBSession.add(following)
+        DBSession.add(follower)
+        DBSession.add(page)
+
+        actual = self._callFUT(follower)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(actual[0].page_id, page.page_id)
+
+    def test__another_user_arrived(self):
+        from questy.models import User, Comment, Page
+        comment = Comment()
+        user1 = User(user_id=1)
+        user2 = User(user_id=2)
+
+        page = Page(page_id=1)
+        page.comments = [comment]
+        user2.comments = [comment]
+
+        DBSession.add(comment)
         DBSession.add(user1)
         DBSession.add(user2)
         DBSession.add(page)
--- questy/user.py
+++ questy/user.py
@@ -2,18 +2,32 @@ from questy.models import (
     DBSession,
     Page,
     Arrival,
+    Comment,
     User,
     follow,
 )
 
 
-def following_stream(user):
-    """ Page stream of following.
+def following_arrival_stream(user):
+    """ Arrived page stream of following users.
     This function returns list of pages which has been arrived
     by people who followed by the 'user'.
     """
     return DBSession.query(Page).\
         join(Arrival).\
+        join(User).\
+        join(follow,
+             (follow.c.following_id == User.user_id) &
+             (follow.c.follower_id == user.user_id)).all()
+
+
+def following_comment_stream(user):
+    """ Commented page stream of following users.
+    This function returns list of pages which has been commented
+    by people who followed by the 'user'.
+    """
+    return DBSession.query(Page).\
+        join(Comment).\
         join(User).\
         join(follow,
              (follow.c.following_id == User.user_id) &
